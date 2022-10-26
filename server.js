2fbbea06451884752b68340c50e78342bc100f39
Sasika <sasikaadesh@gmail.com>
2022-10-26 18:49:57 +0530

    Port changed to 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 require("./db/connectDB");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // import routes
 const userRoutes = require("./routes/user");
@@ -17,5 +17,5 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`);
+  console.log(`Server is running on PORT: ${PORT}`);
 });
